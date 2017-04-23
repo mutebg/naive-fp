07f85158e39b6c129f2f49abc4570a65f74af97c
Stoyan Delev <s.delev@elements.nl>
2017-04-23 12:44:52 +0200

    docs(all): Add anotations and descriptions
    Add anotations and descriptions to all functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+/*
+Helper to log
+
+@sig a -> a
+*/
 export const log = x => {
   /* eslint-disable no-console */
   console.log(x)
@@ -5,11 +10,23 @@ export const log = x => {
   return x
 }
 
+// a -> a
 export const trace = log
 
+/*
+Returns a curried equivalent of the provided function.
+
+@sig: (* -> a) -> (* -> a)
+*/
 export const curry = (fn, arr = []) => (...args) =>
   (a => (a.length === fn.length ? fn(...a) : curry(fn, a)))([...arr, ...args])
 
+/*
+Takes a function and a list, applies the function to each of the list's values,
+and returns a list of the same shape.
+
+@sig: (a -> b) -> [a] -> [b]
+*/
 export const map = curry((fn, list) => {
   if (list.length === 0) {
     return []
@@ -18,8 +35,12 @@ export const map = curry((fn, list) => {
   return [fn(fst)].concat(map(fn, rest))
 })
 
-// export const indexedMap = curry((fn, list) => fasle)
+/*
+Applied to a predicate and a list, returns the list of those elements
+that satisfy the predicate
 
+@sig: (a -> Bool) -> [a] -> [a]
+*/
 export const filter = curry((fn, list) => {
   if (list.length === 0) {
     return []
@@ -32,6 +53,13 @@ export const filter = curry((fn, list) => {
   return filter(fn, rest)
 })
 
+/*
+Returns a single item by iterating through the list, successively calling
+the iterator function and passing it an accumulator value and the current
+value from the array, and then passing the result to the next call.
+
+@sig: (a -> b -> a) -> a -> [b] -> a
+*/
 export const reduce = curry((fn, initValue, list) => {
   if (list.length === 0) {
     return initValue
@@ -40,6 +68,12 @@ export const reduce = curry((fn, initValue, list) => {
   return reduce(fn, fn(initValue, fst), rest)
 })
 
+/*
+Similar to `reduce`, except moves through the input list from
+the right to the left.
+
+@sig: (a -> b -> a) -> a -> [b] -> a
+*/
 export const reduceRight = curry((fn, initValue, list) => {
   if (list.length === 0) {
     return initValue
@@ -48,24 +82,74 @@ export const reduceRight = curry((fn, initValue, list) => {
   return fn(reduceRight(fn, initValue, rest), fst)
 })
 
+/*
+See `reduce`
+
+@sig: (a -> b -> a) -> a -> [b] -> a
+*/
 export const foldl = reduce
 
+/*
+See `reduceRight`
+
+@sig: (a -> b -> a) -> a -> [b] -> a
+*/
 export const foldr = reduceRight
 
+/*
+Returns the first element of the given list.
+
+@sig: [a] -> a
+*/
 export const head = ([fst, ...rest]) => fst
 
+/*
+Returns the last element of the given list.
+
+@sig: [a] -> a
+*/
 export const last = ([fst, ...rest]) => (rest.length === 0 ? fst : last(rest))
 
+/*
+Returns all but the first element of the given list
+
+@sig: [a] -> [a]
+*/
 export const tail = ([fst, ...rest]) => rest
 
+/*
+Performs right-to-left function composition
+
+@sig: [a -> a] -> a -> a
+*/
 export const compose = (...fns) => x => reduceRight((v, fn) => fn(v), x, fns)
 
+/*
+Performs left-to-right function composition
+
+@sig: [a -> a] -> a -> a
+*/
 export const pipe = (...fns) => x => reduce((v, fn) => fn(v), x, fns)
 
+/*
+Returns the result of concatenating the given lists
+
+@sig: [[a]] -> [a]
+*/
 export const concat = (...lists) => reduce((x, y) => x.concat(y), [], lists)
 
+/*
+Returns a new list with the elements in reverse
+
+@sig: [a] -> [a]
+*/
 export const reverse = list => reduceRight((x, s) => [...x, s], [], list)
 
+/*
+Returns the number of elements in a list
+
+@sig: [a] -> Number
+*/
 export const length = list => {
   if (!list.length) {
     return 0
@@ -74,12 +158,33 @@ export const length = list => {
   return 1 + length(rest)
 }
 
+/*
+Returns the larger element in a list
+
+@sig: [Number] -> Number
+*/
 export const max = list => Math.max(...list)
 
+/*
+Returns the smallest element in a list
+
+@sig: [Number] -> Number
+*/
 export const min = list => Math.min(...list)
 
+/*
+The intersperse function takes an element and a list and `intersperses'
+that element between the elements of the list.
+
+@sig: a -> [a] -> [a]
+*/
 export const intersperse = curry((a, list) => list.join(a))
 
+/*
+Returns all but the last element of the given list.
+
+@sig: [a] -> [a]
+*/
 export const init = list => {
   if (length(list) <= 1) {
     return []
@@ -91,11 +196,15 @@ export const init = list => {
 
 /*
 Determines whether any element of the structure satisfies the predicate.
+
+@sig: (a -> Bool) -> [a] -> Bool
 */
 export const any = curry((fn, list) => length(filter(fn, list)) > 0)
 
 /*
 Determines whether all elements of the structure satisfy the predicate.
+
+@sig: (a -> Bool) -> [a] -> Bool
 */
 export const all = curry(
   (fn, list) => length(filter(fn, list)) === length(list),
@@ -103,16 +212,22 @@ export const all = curry(
 
 /*
 The sum function computes the sum of the numbers of a structure.
+
+@sig: [Number] -> Number
 */
 export const sum = list => reduce((x, y) => x + y, 0, list)
 
 /*
-  Multiplies together all the elements of a list.
+Multiplies together all the elements of a list.
+
+@sig: [Number] -> Number
 */
 export const product = list => reduce((x, y) => x * y, 1, list)
 
 /*
-  Returns the first n elements of the given list
+Returns the first n elements of the given list
+
+@sig: Number -> [a] -> [a]
 */
 export const take = curry((position, list) => {
   if (position === 0) {
@@ -126,7 +241,9 @@ export const take = curry((position, list) => {
 })
 
 /*
-  Removes the first n elements of the given list
+Removes the first n elements of the given list
+
+@sig:Number -> [a] -> [a]
 */
 export const drop = curry((position, list) => {
   if (position === 0) {
@@ -140,12 +257,19 @@ export const drop = curry((position, list) => {
 })
 
 /*
-  Splits a given list at a given index.
+Splits a given list at a given index.
+
+@sig: Number -> [a] -> [[a]]
 */
 export const splitAt = curry((position, list) => {
   return [take(position, list), drop(position, list)]
 })
 
+/*
+The permutations function returns the list of all permutations of the argument.
+
+@sig: [a] -> [[a]]
+*/
 export const permutations = list => {
   if (list.length === 0) {
     return [[]]
